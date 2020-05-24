cd5b088abd924633e630694192ee082fda48c338
RadwanDev <radwan.ahmed@tigerit.com>
2020-05-24 17:57:52 +0600

    3 steps color add .

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 (function ($) {
-"use strict";
+    "use strict";
+    var clipboard = new ClipboardJS('.btn-dot');
+
+    clipboard.on('success', function (e) {
+        e.clearSelection();
+        e.trigger.textContent = 'Copied!';
+        var interval = setInterval(timer, 1000);
+
+        function timer() {
+            e.trigger.textContent = e.text;
+            clearInterval(interval);
+        }
+    });
+
+    clipboard.on('error', function (e) {
+        console.info(e);
+    });
 
 
+    $('[data-toggle="tooltip"]').tooltip()
 
-})(jQuery);	
+})(jQuery);
