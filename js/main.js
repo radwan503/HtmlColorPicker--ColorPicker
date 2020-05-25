a45529f335ce10f18ae947a2601eaa7b0099aced
RadwanDev <radwan.ahmed@tigerit.com>
2020-05-25 22:48:12 +0600

    add gradient color

--- js/main.js
+++ js/main.js
@@ -18,6 +18,25 @@
     });
 
 
+
+    var clipboard = new ClipboardJS('.gradient-dot');
+
+    clipboard.on('success', function (e) {
+        e.clearSelection();
+        e.trigger.textContent = 'Copied!';
+        var interval = setInterval(timer, 1000);
+
+        function timer() {
+            e.trigger.textContent = e.text.hide;
+            clearInterval(interval);
+        }
+    });
+
+    clipboard.on('error', function (e) {
+        console.info(e);
+    });
+
+
     $('[data-toggle="tooltip"]').tooltip()
 
 })(jQuery);
